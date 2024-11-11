e65dc0d3508027050310835e8bdcf87eec668a21
d-malhotra2020 <dhruvmalhotra1986@gmail.com>
2024-11-11 10:13:08 -0600

    added dog functionality

--- animal.py
+++ animal.py
@@ -1,9 +1,22 @@
 import sys
 
+def dog():
+    print('Woof!')
+def cat():
+    print('Meow!')
 def default():
     print('Hello')
 def main():
-	default()
+    if sys.argv[1] == 'cat':
+
+
+
+        cat()
+    elif sys.arv[1] == 'dog':
+        dog()
+    else:
+
+        default()
     
 if __name__ == '__main__':
     main()
